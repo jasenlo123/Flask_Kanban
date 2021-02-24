140815fb282ae23c7a10d4d8f08f36abf1761221
Jasen Lo <jasenlo123@gmail.com>
2021-02-23 17:19:43 -0800

    done preclass

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
+    status = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -19,7 +21,9 @@ class Todo(db.Model):
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        task_status = request.form['status']
+
+        new_task = Todo(content=task_content, status = task_status)
 
         try:
             db.session.add(new_task)
@@ -29,8 +33,11 @@ def index():
             return 'There was an issue adding your task'
 
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        to_do_tasks = Todo.query.order_by(Todo.date_created).filter_by(status='To-Do').all()
+        doing_tasks = Todo.query.order_by(Todo.date_created).filter_by(status='Doing').all()
+        done_tasks = Todo.query.order_by(Todo.date_created).filter_by(status='Done').all()
+
+        return render_template('index.html', toDoTasks=to_do_tasks, doingTasks = doing_tasks, doneTasks = done_tasks)
 
 
 @app.route('/delete/<int:id>')
@@ -50,6 +57,7 @@ def update(id):
 
     if request.method == 'POST':
         task.content = request.form['content']
+        task.status = request.form['status']
 
         try:
             db.session.commit()
